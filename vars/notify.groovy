ad4b11a9e4516ccf8674cf21ff852ecd9e0bb1af
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2021-07-11 19:55:06 +0700

    updated notify groovy script for last build info

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -20,8 +20,21 @@ def notify_build(def buildStatus) {
     // https://stackoverflow.com/questions/57364925/jenkins-instance-getitem-fails-for-jobs-in-folders
     def buildName = Jenkins.instance.getItemByFullName('Stage/jenkins-groovy-files')
     if (buildName != null) {
+
 		echo "Last success: ${buildName.getLastSuccessfulBuild()}"
         echo "All builds: ${buildName.getBuilds().collect{ it.getNumber()}}"
         echo "Last build: ${buildName.getLastBuild()}"
+
+		if (buildName.getLastBuild()) {
+			last_job_num = buildName.getLastBuild().getNumber()
+			def upStreamBuild = Jenkins.getInstance().getItemByFullName('Stage/jenkins-groovy-files').getBuildByNumber(last_job_num)
+			println 'LastBuildNumber: ' + last_job_num
+            println "LastBuildTime: ${upStreamBuild.getTime()}"
+
+			if (buildName.getLastSuccessfulBuild()) {
+            	println 'LastSuccessNumber: ' + buildName.getLastSuccessfulBuild().getNumber()
+            	println 'LastSuccessResult: ' + buildName.getLastSuccessfulBuild().result
+			}
+		}
     }
 }
