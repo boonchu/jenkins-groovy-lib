f99e0da66490be96fe5eb6e5b5710338de9b55c8
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2021-07-22 18:18:43 +0700

    DEV-5 updated lib

---
+++ vars/folders.groovy
@@ -0,0 +1,24 @@
+#!groovy
+
+def targetFolders = [
+	'Develop': ['Build', 'Deploy'],
+	'Stage View': ['Build', 'Deploy']
+]
+
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import com.cloudbees.hudson.plugins.folder.*
+
+def call() {
+	jenkins = Jenkins.instance
+	def String[] allJobs = jenkins.getAllItems(Job.class).fullName	
+
+	targetFolders.each { folders -> 
+		def folder = folders.getKey()
+		if (jenkins.getItem(folder) != null) {
+			println "Working at ${folder}"
+		}
+	}
+}
