512b9514247a4693aa2798d341709eff0e2b140b
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2021-07-22 19:48:17 +0700

    DEV-7: Exposed job info

--- vars/folders.groovy
+++ vars/folders.groovy
@@ -9,7 +9,7 @@ import com.cloudbees.hudson.plugins.folder.*
 def call() {
     def targetFolders = [
     	'Develop': ['Build', 'Deploy'],
-    	'Stage View': ['Build', 'Deploy']
+    	'Stage': ['Build', 'Deploy']
     ]
 
 	def jenkins = Jenkins.instance
@@ -19,6 +19,15 @@ def call() {
 		def folder = folders.getKey()
 		if (jenkins.getItem(folder) != null) {
 			println "Working at ${folder}"
+			def custom_jobs = targetFolders[folder]
+			println "Working at ${custom_jobs}"
+			
+			def existingJobs = allJobs.findAll { job -> job.contains(folder) }
+			targetJobs.each { job ->
+				if (existingJobs.any { existingJob -> existingJob.contains(job) }) {
+					println "   Working at ${job.split('/')[0]}"
+				}
+			}
 		}
 	}
 }
