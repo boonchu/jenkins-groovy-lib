fbfd02108cdcc3d24230ba93c6d80465c6332ee5
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2021-08-04 00:49:31 +0700

    DEV-7: updated read_artifact_info()

--- vars/read_artifact_info.groovy
+++ vars/read_artifact_info.groovy
@@ -4,13 +4,16 @@ def call() {
     def pom = readMavenPom file: 'pom.xml'
 
     // https://maven.apache.org/pom.html
-    env.ARTIFACT_VERSION = pom.getVersion()
-    env.ARTIFACT_PKG_NAME = pom.getArtifactId()
-    env.ARTIFACT_PKG_SUFFIX = pom.getPackaging()
+    env.ARTIFACT_PKG_VERSION = pom.version
+    env.ARTIFACT_PKG_NAME = pom.artifactId
+    env.ARTIFACT_PKG_SUFFIX = pom.packaging
+    env.ARTIFACT_PKG_GROUP = pom.groupId
   
-    echo "CALL->INFO : ARTIFACT_VERSION is ${env.ARTIFACT_VERSION}"
+    echo "CALL->INFO : ARTIFACT_PKG_VERSION is ${env.ARTIFACT_PKG_VERSION}"
     echo "CALL->INFO : ARTIFACT_PKG_NAME is ${env.ARTIFACT_PKG_NAME}"
     echo "CALL->INFO : ARTIFACT_PKG_SUFFIX is ${env.ARTIFACT_PKG_SUFFIX}"
+    echo "CALL->INFO : ARTIFACT_PKG_GROUP is ${env.ARTIFACT_PKG_GROUP}"
+
     sh "echo ${pom.version} > .git/tagName"
     tagName = readFile('.git/tagName')
     echo "CALL->INFO: value read from file .git/tagName as ${tagName}"
