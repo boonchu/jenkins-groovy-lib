59d0248739d271f9f640749fc5cf64f2808a5bd8
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2021-08-04 01:44:36 +0700

    DEV-7 Jenkinsfile updated to support artifact discovery

--- vars/read_artifact_info.groovy
+++ vars/read_artifact_info.groovy
@@ -4,15 +4,18 @@ def call() {
     def pom = readMavenPom file: 'pom.xml'
 
     // https://maven.apache.org/pom.html
-    env.ARTIFACT_PKG_VERSION = pom.version
-    env.ARTIFACT_PKG_NAME = pom.artifactId
-    env.ARTIFACT_PKG_SUFFIX = pom.packaging
-    env.ARTIFACT_PKG_GROUP = pom.groupId
-  
-    echo "CALL->INFO : ARTIFACT_PKG_VERSION is ${env.ARTIFACT_PKG_VERSION}"
-    echo "CALL->INFO : ARTIFACT_PKG_NAME is ${env.ARTIFACT_PKG_NAME}"
-    echo "CALL->INFO : ARTIFACT_PKG_SUFFIX is ${env.ARTIFACT_PKG_SUFFIX}"
-    echo "CALL->INFO : ARTIFACT_PKG_GROUP is ${env.ARTIFACT_PKG_GROUP}"
+    // https://stackoverflow.com/questions/53541489/updating-environment-global-variable-in-jenkins-pipeline-from-the-stage-level/53541813
+    withEnv([
+		"ARTIFACT_PKG_VERSION=${pom.version}",
+		"ARTIFACT_PKG_NAME=${pom.artifactId}",
+        "ARTIFACT_PKG_SUFFIX=${pom.packaging}",
+        "ARTIFACT_PKG_GROUP=${pom.groupId}"
+	]) {
+    	echo "CALL->INFO : ARTIFACT_PKG_VERSION is ${env.ARTIFACT_PKG_VERSION}"
+    	echo "CALL->INFO : ARTIFACT_PKG_NAME is ${env.ARTIFACT_PKG_NAME}"
+    	echo "CALL->INFO : ARTIFACT_PKG_SUFFIX is ${env.ARTIFACT_PKG_SUFFIX}"
+    	echo "CALL->INFO : ARTIFACT_PKG_GROUP is ${env.ARTIFACT_PKG_GROUP}"
+	}
 
     sh "echo ${pom.version} > .git/tagName"
     tagName = readFile('.git/tagName')
