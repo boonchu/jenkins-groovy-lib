284fdcb88ae64faa8ff77af64ddce1034ba8b6a7
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2022-05-07 11:22:39 -0700

    1. updated main.py 2. updated libraries 3. added new line to libraries branch_b 4. rebase branch_a to branch_b with main.py context changes.

---
+++ libraries.py
@@ -0,0 +1,2 @@
+add new files to this branch_b #1
+add new files to this branch_b #2
--- main.py
+++ main.py
@@ -6,4 +6,11 @@ from acl import os
 This is a comment that was written in
 more than just a one line.
 """
+
+<<<<<<< HEAD
+print("Say Hello First!")
+=======
+
+print("I do not want to say anything today")
+>>>>>>> dc1fe8b... 1. updated main.py
 print("Hello World")
