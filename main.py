54374eba6d5ce5b3a1fcfd7ee02dca575074a812
Boonchu Ngampairojpibul <b.ngampairojpibul@accenture.com>
2022-05-07 12:01:29 -0700

    updated main.py from branch_a

--- main.py
+++ main.py
@@ -7,10 +7,5 @@ This is a comment that was written in
 more than just a one line.
 """
 
-<<<<<<< HEAD
 print("Say Hello First!")
-=======
-
-print("I do not want to say anything today")
->>>>>>> dc1fe8b... 1. updated main.py
 print("Hello World")
